c7f88a399b2b79c9ddbcc89e164cb44271ff84ab
Eugen Guriev <gurievcreative@gmail.com>
2024-04-29 22:30:37 +0300

    refactor: mongo.ts to conditionally log test access token

--- plugins/mongo.ts
+++ plugins/mongo.ts
@@ -5,7 +5,9 @@ export default defineNitroPlugin(async () => {
   console.info('🚚 Connecting...', mongoUri)
   await connect(mongoUri)
   console.info('Connected to MongoDB 🚀', mongoUri)
-  console.log('=========================================')
-  console.log('🔑 Test acces token: ', issueAccessToken({ userId: '123' }, { secret: 'secret', expiresIn: '9000m' }))
-  console.log('=========================================')
+  if (process.env.MODE === 'test') {
+    console.log('=========================================')
+    console.log('🔑 Test acces token: ', issueAccessToken({ userId: '123' }, { secret: 'secret', expiresIn: '9000m' }))
+    console.log('=========================================')
+  }
 })
