2318214fea610718589e927ba9fc2f403138b57e
Eugen Guriev <gurievcreative@gmail.com>
2024-04-29 22:17:26 +0300

    refactor: api test file and add routes and models for tokens, wallets, and transactions

--- test-api/api.test.ts
+++ test-api/api.test.ts
@@ -8,7 +8,7 @@ describe('API', () => {
     address: ''
   }
 
-  describe('GET /wallet', () => {
+  describe('/wallet', () => {
     it('[200] generate new wallet', async () => {
       await $fetch('/wallet', {
         baseURL: 'http://localhost:3000',
@@ -28,7 +28,6 @@ describe('API', () => {
     })
 
     it('[200] get ballance', async () => {
-      console.log('wallet', `/wallet/${wallet.address}`)
       await $fetch(`/wallet/${wallet.address}`, {
         baseURL: 'http://localhost:3000',
         headers: {
@@ -42,6 +41,63 @@ describe('API', () => {
             incomeBySymbol: expect.any(Object),
             outcome: expect.any(Array),
             outcomeBySymbol: expect.any(Object)
+          })
+        }
+      })
+    })
+  })
+
+  describe('/token', () => {
+    it('[200] get all tokens', async () => {
+      await $fetch('/token', {
+        baseURL: 'http://localhost:3000',
+        headers: {
+          Accept: 'application/json'
+        },
+        onResponse: ({ response }) => {
+          expect(response.status).toBe(200)
+          expect(response._data).toBeInstanceOf(Array)
+        }
+      })
+    })
+
+    it('[200] create token', async () => {
+      await $fetch('/token', {
+        method: 'POST',
+        baseURL: 'http://localhost:3000',
+        headers: {
+          Accept: 'application/json',
+          Cookie: `accessToken=${accessToken}`
+        },
+        body: {
+          name: 'Test Token',
+          symbol: 'TST',
+          wallet: wallet.address,
+          emission: 1000
+        },
+        onResponse: ({ response }) => {
+          expect(response.status).toBe(200)
+          expect(response._data).toMatchObject({
+            name: 'Test Token',
+            symbol: 'TST',
+            author: '55555'
+          })
+        }
+      })
+    })
+
+    it('[200] token successfully added', async () => {
+      await $fetch('/token', {
+        baseURL: 'http://localhost:3000',
+        headers: {
+          Accept: 'application/json'
+        },
+        onResponse: ({ response }) => {
+          expect(response.status).toBe(200)
+          expect(response._data[0]).toMatchObject({
+            name: 'Test Token',
+            symbol: 'TST',
+            author: '55555'
           })
         }
       })
