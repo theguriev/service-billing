944d3c59ee1aa7356b22c963cf6f51fc7dd9080c
Eugen Guriev <gurievcreative@gmail.com>
2024-04-30 20:29:21 +0300

    refactor: wallet route to return entire wallet object in index.ts

--- routes/wallet/index.ts
+++ routes/wallet/index.ts
@@ -2,8 +2,5 @@ import { Wallet } from 'ethers'
 
 export default eventHandler(() => {
   const wallet = Wallet.createRandom()
-  return {
-    privateKey: wallet.privateKey,
-    address: wallet.address
-  }
+  return wallet
 })
