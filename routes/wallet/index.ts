248ebe7fb1e7804c50a99841c45510eae4c93850
Eugen Guriev <gurievcreative@gmail.com>
2024-04-30 20:57:50 +0300

    refactor: wallet route to include privateKey in index.ts

--- routes/wallet/index.ts
+++ routes/wallet/index.ts
@@ -2,5 +2,8 @@ import { Wallet } from 'ethers'
 
 export default eventHandler(() => {
   const wallet = Wallet.createRandom()
-  return wallet
+  return {
+    ...wallet,
+    privateKey: wallet.privateKey
+  }
 })
