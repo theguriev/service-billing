62da0f7038fb19ed8c55efc33fa65761793a828b
Eugen Guriev <gurievcreative@gmail.com>
2024-05-20 21:27:24 +0300

    refactor: update token route to include pagination and sorting options

--- routes/token/index.ts
+++ routes/token/index.ts
@@ -1,1 +1,4 @@
-export default eventHandler(() => ModelToken.find())
+export default eventHandler((event) => {
+  const { limit = 10, offset = 0, orderBy = 'timestamp', order = 'desc' } = getQuery(event)
+  return ModelToken.find().sort({ [String(orderBy)]: order === 'asc' ? 1 : -1 }).limit(Number(limit)).skip(Number(offset))
+})
