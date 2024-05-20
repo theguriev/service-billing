6b4eea3f0b260bfbb54c29b8c51c304b62c038cc
Eugen Guriev <gurievcreative@gmail.com>
2024-05-20 22:11:17 +0300

    refactor: remove wallet route and related files

--- routes/wallet/index.ts
+++
@@ -1,9 +0,0 @@
-import { Wallet } from 'ethers'
-
-export default eventHandler(() => {
-  const wallet = Wallet.createRandom()
-  return {
-    ...wallet,
-    privateKey: wallet.privateKey
-  }
-})
